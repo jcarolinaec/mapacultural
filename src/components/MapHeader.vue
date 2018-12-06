c2401e64fd4d87a99af7a019d2f8b18f2d376f86
carogomezt <carolina9511@gmail.com>
2018-12-05 23:58:30 -0500

    Fixing responsive

--- src/components/MapHeader.vue
+++ src/components/MapHeader.vue
@@ -1,29 +1,42 @@
 <template>
-    <header class="Header">
-      <nav class="Navigation">
-        <!--
-        <router-link to="/">Home</router-link> |
-        <router-link to="/about">About</router-link>
-        -->
-        <div class="row">
-          <ul class="Navigation-main">
-            <li class="Navigation-mainItem"><a class="Navigation-mainItemLink" href="#">Lugares</a></li>
-            <li class="Navigation-mainItem"><a class="Navigation-mainItemLink" href="#">Organizaciones</a></li>
-            <li class="Navigation-mainItem"><a class="Navigation-mainItemLink" href="#">Personas</a></li>
-            <li class="Navigation-mainItem"><a class="Navigation-mainItemLink" href="#">Circuitos</a></li>
-            <li class="Navigation-mainItem"><a class="Navigation-mainItemLink" href="#">Densidades</a></li>
-          </ul>
-          <ul class="Navigation-second">
-            <li class="Navigation-secondItem"><a href="#" class="Navigation-secondItemLink">Estadisticas</a></li>
-            <li class="Navigation-secondItem"><a href="#" class="Navigation-secondItemLink">Documentos</a></li>
-            <li class="Navigation-secondItem"><a href="#" class="Navigation-secondItemLink">Cronicas de Rutas</a></li>
-            <li class="Navigation-secondItem"><a href="#" class="Navigation-secondItemLink">Directorio</a></li>
-          </ul>
-        </div>
-        
-        
-      </nav>
-    </header>
+  <header>
+    <b-navbar toggleable="md" type="dark" variant="info" class="Header">
+    <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+    <b-navbar-brand href="#">
+      <picture>
+        <img class="ImgBrand" src="../assets/logo-mapa.png" alt="Logo">
+      </picture>
+    </b-navbar-brand>
+    <b-navbar-nav right class="SponsorsLogo">
+      <picture>
+        <img class="ImgSponsors" src="../assets/logos-sponsors.png" alt="Logo Sponsors">
+      </picture>
+    </b-navbar-nav>
+    <b-collapse is-nav id="nav_collapse">
+      <b-navbar-nav class="Navigation-main">
+        <b-nav-item class="Navigation-mainItem Navigation-mainItemLink" href="#">Lugares</b-nav-item>
+        <b-nav-item class="Navigation-mainItem Navigation-mainItemLink" href="#">Organizaciones</b-nav-item>
+        <b-nav-item class="Navigation-mainItem Navigation-mainItemLink" href="#">Personas</b-nav-item>
+        <b-nav-item class="Navigation-mainItem Navigation-mainItemLink" href="#">Circuitos</b-nav-item>
+        <b-nav-item class="Navigation-mainItem Navigation-mainItemLink" href="#">Densidades</b-nav-item>
+      </b-navbar-nav>
+
+      <!-- Right aligned nav items -->
+      <b-navbar-nav class="ml-auto Navigation-second">
+        <b-nav-item class="Navigation-secondItem" right> Estadisticas
+        </b-nav-item>
+        <b-nav-item class="Navigation-secondItem" right> Documentos
+        </b-nav-item>
+        <b-nav-item class="Navigation-secondItem" right> Cronicas de Rutas
+        </b-nav-item>
+        <b-nav-item class="Navigation-secondItem" right> Directorio
+        </b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</header>
+
+    
 </template>
 
 <script>
@@ -41,13 +54,12 @@ $light-blue: #35acaf;
 
 .Header {
   background-color: $orange;
-  background: url(../assets/navbar.png) no-repeat center center; 
+  background: url(../assets/banner.png) no-repeat center center; 
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
   background-size: cover;
-  // padding: 1.5em 3em;
-  padding: 3.5em 3em 3.5em 3em;
+  padding: 1.5em 1em !important;
   position: relative;
   margin: 0;
   max-width: 100%;
@@ -91,13 +103,17 @@ $light-blue: #35acaf;
     padding: 0;
     margin: 0;
 
+    @media (max-width: 767px) {
+      display: contents !important;
+    }
+
     &Item {
       position: relative;
       background-color: $gray;
       border: 1px solid white;
       margin: 0 3px;
       padding: 2px 4px 2px 12px;
-      min-width: 150px;
+      min-width: 130px;
       text-align: center;
       &::before {
         content: '';
@@ -118,6 +134,10 @@ $light-blue: #35acaf;
         background-color: $light-blue;
         font-family: 'Barlow Semi Condensed Medium', sans-serif;
       }
+      a {
+        padding: 0;
+      }
+
     }
 
     &ItemLink {
@@ -131,22 +151,26 @@ $light-blue: #35acaf;
         color: white;
       }
     }
-
-
   }
 
   &-second {
-    display: inline-block;
+    display: inline-block !important;
     list-style: none;
     position: absolute;
-    top: 0;
-    right: 2em;
-    
-    &ItemLink {
-      color: white;
+    top: 10%;
+    right: 1em;
+
+    @media (max-width: 767px) {
+      display: contents !important;
+    }
+
+    a {
+      color: white !important;
       text-decoration: none;
       font-family: 'Barlow Semi Condensed Thin', sans-serif;
       font-size: 16px;
+      margin-top: -35px;
+
     }
 
     & li::before {
@@ -157,6 +181,24 @@ $light-blue: #35acaf;
       width: 1em;
       margin-left: -1em;
     }
+  }
+}
+
+.ImgBrand {
+  width: 90%;
+}
+
+.ImgSponsors {
+  width: 70%;
+}
+.SponsorsLogo {
+  
+  @media (min-width: 768px) and (max-width: 1024px) {
+    margin-left: 20%;
+  }
+
+  @media (min-width: 1025px) {
+    margin-left: 28%;
   }
 }
 </style>
